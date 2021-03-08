5db9dba854d77e86924b23ec61874114fc4aa8a9
jordengerovac <jordengerovac@gmail.com>
2021-03-07 19:14:45 -0500

    Updating unsplash api call header

--- src/actions/photoActions.js
+++ src/actions/photoActions.js
@@ -3,7 +3,12 @@ import { FETCH_PHOTO, TOGGLE_PHOTO, SET_DEFAULT_PHOTO } from './types';
 export const fetchPhoto = () => dispatch => {
   const UNSPLASH_API_KEY = `${process.env.REACT_APP_UNSPLASH_API_KEY}`;
 
-  fetch("https://api.unsplash.com/photos/random/?collections=19428159&client_id=" + UNSPLASH_API_KEY)
+  fetch("https://api.unsplash.com/photos/random/?collections=19428159", {
+    method: "GET",
+    headers: {
+      "Authorization": "Client-ID " + UNSPLASH_API_KEY
+    }
+  })
   .then(res => res.json())
   .then(
     (result) => {
